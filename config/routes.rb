89a96539699131c141f4f7616fa6548ff96aa24e
Artem Slabodnik <artem.slabodnik@gmail.com>
2012-12-18 14:48:26 +0300

    delete some path, add path to authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Photo::Application.routes.draw do
-  get "pictures/controller"
-
-  get "galleries/controller"
-
-  root :to => "welcome#page"
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  
   devise_for :users do
-    get "/", :to => "devise/registrations#new"
     match '/home' => "application#home", as: :user_root
   end
+
+  authenticated :user do
+    root to: 'application#home'
+  end
+
+  root :to => "welcome#page"
 
   resources :galleries do
     resources :pictures do
